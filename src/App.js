4fee528969368ebe237fcf80e127cbbe1adb75b9
Pavel <mavelichwebdev@gmail.com>
2022-03-17 01:27:32 +0500

    Added React course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Count from './components/Counter';
+import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'Description'},
+        {id: 2, title: 'C++', body: 'Description'},
+        {id: 3, title: 'R', body: 'Description'},
+        {id: 4, title: 'Python', body: 'Description'}
+    ])
+
+    const [title, setTitle] = useState('');
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        console.log(title);
+    }
+
+    return (
+        <div className='App'>
+            <form>
+                {/* Управляемый компонент */}
+                <MyInput
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type='text'
+                    placeholder='Название поста'
+                />
+                <MyInput type='text' placeholder='Описание поста' />
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title='Список постов' />
+        </div>
+    )
 }
 
 export default App;
