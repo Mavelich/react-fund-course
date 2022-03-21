f4c0f85ec464115c3cd7f7ce1229d7569548267f
Pavel <mavelichwebdev@gmail.com>
2022-03-21 23:31:56 +0500

    Added components, code optimization

--- src/App.js
+++ src/App.js
@@ -1,53 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './styles/App.css';
-import PostList from './components/PostList';
-import PostForm from './components/PostForm';
-import MySelect from './components/UI/select/MySelect';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './pages/About';
+import Posts from './pages/Posts';
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'dd', body: 'аа'},
-        {id: 2, title: 'cc', body: 'бб'},
-        {id: 3, title: 'bb', body: 'вв'},
-        {id: 4, title: 'aa', body: 'гг'}
-    ])
-
-    const [selectedSort, setSelectedSort] = useState('');
-
-    const createPost = (newPost) => {
-        setPosts([...posts, newPost])
-    }
-
-    // Получаем post из дочернего элемента
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
-
-    const sortPosts = (sort) => {
-        setSelectedSort(sort);
-        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
-    }
-
+    
     return (
-        <div className='App'>
-            <PostForm create={createPost} />
-            <hr style={{margin: '15px 0'}} />
-            <MySelect
-                value={selectedSort}
-                onChange={sortPosts}
-                defaultValue='Сортировка'
-                options={[
-                    {value: 'title', name: 'По названию'},
-                    {value: 'body', name: 'По описанию'}
-                ]}
-            />
-            {posts.length !== 0
-                ? <PostList remove={removePost} posts={posts} title='Список постов' />
-                : <h1 style={{textAlign: 'center'}}>
-                    Посты не найдены!
-                </h1>
-            }
-        </div>
+        <BrowserRouter>
+            <div className='navbar'>
+                <div className='navbar__links'>
+                    <a href='about'>О сайте</a>
+                    <a href='posts'>Посты</a>
+                </div>
+            </div>
+            <Routes>
+                <Route path="about" element={<About />} />
+            </Routes>
+            <Routes>
+                <Route path="posts" element={<Posts />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,80 @@
+import React, {useState, useMemo, useEffect} from 'react';
+import '../styles/App.css';
+import PostList from '../components/PostList';
+import PostForm from '../components/PostForm';
+import MyInput from '../components/UI/input/MyInput';
+import PostFilter from '../components/PostFilter';
+import MyModal from '../components/UI/modal/MyModal';
+import MyButton from '../components/UI/button/MyButton';
+import { usePosts } from '../hooks/usePosts';
+import PostServise from '../API/PostService';
+import Loader from '../components/UI/loader/Loader';
+import { useFetching } from '../hooks/useFetching';
+import { getPageCount, getPagesArray } from '../utils/pages';
+import Pagination from '../components/UI/pagination/Pagination';
+
+function Posts() {
+    const [posts, setPosts] = useState([]);
+    const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
+    const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+        const response = await PostServise.getAll(limit, page);
+        setPosts(response.data);
+        const totalCount = (response.headers['x-total-count']);
+        setTotalPages(getPageCount(totalCount, limit));
+    })
+
+    useEffect(() => {
+        fetchPosts(limit, page);
+    }, []);
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost]);
+        setModal(false);
+    }
+
+    // Получаем post из дочернего элемента
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const changePage = (page) => {
+        setPage(page);
+        fetchPosts(limit, page);
+    }
+
+    return (
+        <div className='App'>
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Создать пользователя
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost} />
+            </MyModal>
+            <hr style={{margin: '15px 0'}} />
+            <PostFilter
+                filter = {filter}
+                setFilter = {setFilter}
+            />
+            {postError &&
+                <h1>Произошла ошибка! {postError}</h1>
+            }
+            {isPostsLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader /></div>
+                : <PostList remove={removePost} posts={sortedAndSearchPosts} title='Список постов' />
+            }
+            <Pagination 
+                page={page} 
+                changePage={changePage} 
+                totalPages={totalPages}
+            />
+        </div>
+    )
+}
+
+export default Posts;
